cc0d68b56889e5de6c7acc4d1380afeec46badc1
zhaoweijie <949210784@qq.com>
2021-11-03 10:22:38 +0800

    新建

---
+++ SqlEntityTool.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SqlEntityTool.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ SqlEntityTool.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SqlEntityTool.Service
+{
+    public class Class1
+    {
+    }
+}
